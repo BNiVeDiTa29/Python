6171a101450128d9618d3adac0ba597d46fdca42
Nivedita Boyina <148596813+BNiVeDiTa29@users.noreply.github.com>
2026-03-11 18:21:28 -0700

    Create reversearray.py

---
+++ reversearray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseArray(self, nums: list[int]) -> None:
+        # TODO
+        
+
+                i = 0
+                j = len(nums)-1
+                    
+                while i<=j:
+
+                        temp = nums[i]
+                        nums[i] = nums[j]
+                        nums[j] = temp
+
+                        i=i+1
+                        j = j-1
+    pass
